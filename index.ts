d05c2813139ed58b17ff489408b91ca6d8c4c40e
apine <1985486089@qq.com>
2023-09-17 15:07:19 +0800

    🔥 : 移除 demo

--- index.ts
+++
@@ -1,7 +0,0 @@
-import { type } from './typing'
-
-const ele = document.querySelector('#demo')
-
-type(ele, ['hello', 1000, 'hello world', 1000, 'hello typical'], 3, () => {
-    console.log('finish')
-})
